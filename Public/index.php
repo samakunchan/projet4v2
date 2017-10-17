85e86050dd7078d0889bcfe58ce040cae45edfd7
Samakunchan <samakunchan@gmail.com>
2017-10-17 15:38:03 +0200

    Intégration des pages

--- Public/index.php
+++ Public/index.php
@@ -9,10 +9,25 @@ require '../Modele/App/App.php';
 require '../Modele/Entity/Articles.php';
 require '../Modele/Manager/ManagerDonnees.php';
 require '../Modele/Manager/ManagerArticles.php';
-use Modele\Manager\ManagerArticles;
-use Modele\Entity\Articles;
 
-$test = new ManagerArticles();
-$test->readAll();
-$articles = new Articles();
+
+if(isset($_GET['page'])){
+    $pages = $_GET['page'];
+}else{
+    $pages = 'accueil';
+}
+
+ob_start();
+if($pages === 'acceuil'){
+    require '../Vue/Pages/accueil.php';
+}elseif ($pages=== 'single'){
+    require '../Vue/Pages/single.php';
+}elseif ($pages=== 'categorie'){
+    require '../Vue/Pages/categories.php';
+}else{
+    echo 'Un problème est survenu dans le chargement de la page - Voir Index.php';
+}
+$contenu = ob_get_clean();
+require '../Vue/Gabarit/gabarit.php';
+
 
--- Vue/Gabarit/gabarit.php
+++ Vue/Gabarit/gabarit.php
@@ -1,7 +1,30 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Samakunchan
- * Date: 17/10/2017
- * Time: 15:27
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Starter Template for Bootstrap</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+</nav>
+
+<div class="container" style="padding-top: 100px;">
+
+    <div class="starter-template">
+        <?php echo $contenu;?>
+    </div>
+
+</div>
+</body>
+</html>
