cd51062d1940eb7621329da6319c5b9def1f1ac2
Samakunchan <samakunchan@gmail.com>
2017-10-18 13:29:58 +0200

    Listes des chapitres OK

--- Controlleur/ControlleurChapitres.php
+++ Controlleur/ControlleurChapitres.php
@@ -8,8 +8,15 @@
 
 namespace Controlleur;
 
-
+use Modele\Manager\ManagerArticles;
+use Vue\Core\Vue;
 class ControlleurChapitres
 {
-
+    public function listeChaptitres()
+    {
+        $chapitres = new ManagerArticles();
+        $donnees = $chapitres->readAll();
+        $pages = new Vue('chapitres');
+        $pages->genererPages($donnees);
+    }
 }
--- Vue/Pages/chapitres.php
+++ Vue/Pages/chapitres.php
@@ -1,7 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Samakunchan
- * Date: 18/10/2017
- * Time: 12:05
- */
+<div class="row chapitres">
+    <h1>Liste de tout les chapitres</h1>
+    <div class="col-lg-12">
+        <ul>
+            <?php foreach ($donnees as $donnee) :; ?>
+                <li>
+                    <p>
+                        <a href="index.php?page=articles&id=<?php echo $donnee->getId();?>">
+                            <?php echo $donnee->getTitre();?>
+                        </a>
+                        <span class="col-lg-offset-1">
+                            Articles créé le : <?php echo $donnee->getDateCreation()?>
+                        </span>
+                    </p>
+
+                </li>
+
+            <?php endforeach; ?>
+        </ul>
+    </div>
+</div>
